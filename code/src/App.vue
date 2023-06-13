b4b3f7d4f44d1de51815279c76486e92a785495c
moisesPompilio <moisesjtmed@gmail.com>
2023-06-13 20:53:45 -0300

    feat: adicionamento do page HomePage no App.vue

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <BestTransport/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
 import BestTransport from './components/BestTransport.vue'
+import HomePage from './page/HomePage/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    BestTransport
+    BestTransport,
+    HomePage
   }
 }
 </script>
