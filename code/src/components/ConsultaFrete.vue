b76a11beec2934cffc5b4f17bf1fcd83255535f2
moisesPompilio <moisesjtmed@gmail.com>
2023-06-13 20:58:57 -0300

    feat: criação do componente ConsultaFrete para ele  fazer a consulta do frete

---
+++ code/src/components/ConsultaFrete.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <form @submit.prevent="buscarFrete()">
+      <fieldset>
+        <legend class="negrito">
+          <i class="bi bi-map"></i> Insira o destino e o peso
+        </legend>
+        <div class="mb-3">
+          <label for="selectCidade" class="form-label negrito">Destino</label>
+          <select
+            class="form-select"
+            id="selectCidade"
+            v-model="cidade"
+            required
+          >
+            <option disabled selected value="">Selecione um destino</option>
+            <option
+              v-for="cidade in $store.state.Cities"
+              :key="cidade"
+              :value="cidade"
+            >
+              {{ cidade }}
+            </option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <label for="peso" class="form-label negrito">Peso</label>
+          <input
+            type="number"
+            id="peso"
+            v-model="peso"
+            class="form-control"
+            placeholder="Peso da carga em Kg"
+            required
+          />
+        </div>
+        <div class="center">
+          <button type="submit" class="btn btn-primary">Analisar</button>
+        </div>
+      </fieldset>
+    </form>
+  </div>
+</template>
+
+<script>
+import { BuscarFretes } from "@/services/FreightQuote/BuscarFretes";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+export default {
+  components: {},
+  data() {
+    const peso = "";
+    const cidade = "";
+
+    return {
+      peso,
+      cidade,
+    };
+  },
+  methods: {
+    buscarFrete() {
+      BuscarFretes(this.cidade, this.peso);
+    },
+  },
+};
+</script>
+<style scoped>
+.card {
+  background-color: #e2dcdc;
+  padding: 80px 20px;
+}
+.negrito {
+  font-weight: bold;
+}
+</style>
